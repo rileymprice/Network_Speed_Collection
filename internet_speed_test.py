0fc5b96ae2879ba04585cf6b0750d4ccd760ec85
Riley Price <rileymprice@gmail.com>
2019-01-06 16:33:29 -0500

    Script with Logging
    Added logging to run on the Pi. Scheduled with CRON to run every hour.

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -2,39 +2,73 @@ import speedtest
 from datetime import datetime as dt
 from path import Path
 from os import path
+import logging
+
+logger = logging.getLogger('Speedtest')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('./logs/speedtest.log')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def test_speed():
-    '''Input: N/A
+    '''Goal: Call speedtest and return the results dictionary
+    Input: N/A
     Output: Results dictionary
     '''
-    test = speedtest.Speedtest()
-    test.get_best_server()
-    test.download()
-    test.upload()
-    return test.results.dict()
+    logger.info('Calling Speedtest')
+    try:
+        test = speedtest.Speedtest()
+    except Exception as e:
+        logger.critical('Error with Speedtest: %s', e)
+        return None
+    else:
+        logger.info('Speedtest Successful')
+        test.get_best_server()
+        test.download()
+        test.upload()
+        logger.debug('Returning from test_speed %s', test.results)
+        return test.results.dict()
 
 
 def write_to_file(file_name, given_string):
-    '''Input: file to write, string to write to file
+    '''Goal: Given a file name and string, creates a file with the string or appends the string to the file
+    Input: file to write, string to write to file
     Output: Depending if file exists, this is create file and write string to file, otherwise will append string to file
     '''
-    print('Printing to file')
+    logger.info('Writing to file')
     if path.exists(Path(f'./{file_name}')):
-        print('Appending to file')
+        logger.debug('File already exists in write_to_file: %s', file_name)
         with open(Path(f'./{file_name}'), 'a') as appendable_file:
             appendable_file.write(given_string+'\n')
     else:
-        print('Creating File')
+        logger.debug('File does not exist in write_to_file: %s', file_name)
         with open(Path(f'./{file_name}'), 'w') as writable_file:
+            # Headers are:
+            # DATE
+            # TIME
+            # DOWNLOAD
+            # UPLOAD
+            # PING / LATENCY
+            # IP
+            # BYTES SENT
+            # BYTES RECEIVED
+            # ISP
+            # SERVER LATITUDE
+            # SERVER LONGITUDE
+            # ISP LATITUDE
+            # ISP LONGITUDE
+            # PROCESS TIME (time to call speedtest)
             writable_file.write(
                 'DATE,TIME,DOWNLOAD,UPLOAD,PING/LATENCY,IP,BYTES_SENT,BYTES_RECEIVED,ISP,SERVER_LAT,SERVER_LON,ISP_LAT,ISP_LON,PROCESS_TIME\n')
             writable_file.write(given_string+'\n')
-    print('Wrote to file')
+    logger.info('Wrote to file')
 
 
 def speed_to_string(internet_test, time_called):
-    print('Printing to String')
+    logger.info('Printing to string speed_to_string')
     result = ''
     result += time_called.strftime('%m-%d-%y')+','  # date
     result += time_called.strftime('%H:%M')+','  # time
@@ -51,21 +85,26 @@ def speed_to_string(internet_test, time_called):
     result += str(internet_test['server']['lon'])+','  # server lon
     result += str(internet_test['client']['lat'])+','  # isp lat
     result += str(internet_test['client']['lon'])+','  # isp lon
-
+    logger.debug('Returning from speed_to_string %s', result)
     return result
 
 
 def main():
+    logger.info('Started Script')
     time_called = dt.now()
-    print('Starting test')
     start_time = dt.now()
     internet_test = test_speed()
     end_time = dt.now()
     process_time = end_time - start_time
-    print('Test complete')
-    result_string = speed_to_string(internet_test, time_called)
-    result_string += str(process_time)
-    write_to_file('internet_speed.csv', result_string)
+    if internet_test == None:
+        logger.debug('Speedtest failed, cancelling rest of script')
+    else:
+        logger.info('Test Complete')
+        result_string = speed_to_string(internet_test, time_called)
+        result_string += str(process_time)
+        write_to_file('internet_speed.csv', result_string)
+        logger.info('Script Complete')
+        logger.info('-'*50)
 
 
 if __name__ == "__main__":
